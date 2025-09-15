5afec1185d7d45e2d8610d0169b443e4b2ce5abe
Khatia Sikharulidze <sikharulidzekhatiaa@gmail.com>
2025-09-15 17:23:40 -0400

    feat: add CategoryTiles component and update kimano-ex image to pajama

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import { generateLocalizedMetadata } from "@/utils/metadata/generateMetadata";
 import Hero from "@/components/home/Hero";
 import StorySlice from "@/components/home/StorySlice";
+import CategoryTiles from "@/components/home/CategoryTiles";
 export async function generateMetadata(ctx: {
   params: Promise<{ locale: string }>;
 }) {
@@ -11,6 +12,7 @@ export default function Home() {
     <div className=" bg-[#FDFBF9] overflow-hidden">
       <Hero />
       <StorySlice />
+      <CategoryTiles/>
     </div>
   );
 }
